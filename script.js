86cfba553aa82d9aa9c59acd859972e34449b091
Cécile Phan Nguyen <cecilephan@outlook.com>
2023-11-19 23:25:10 +0100

    implantation loader js

---
+++ script.js
@@ -0,0 +1,16 @@
+window.onload = function () {
+    setTimeout(function () {
+        let opacity = 1;
+        let interval = setInterval(function () {
+            if (opacity <= 0.1) {
+                clearInterval(interval);
+                document.querySelector(".loader").style.display = "none";
+            }
+            document.querySelector(".loader").style.opacity = opacity;
+            document.querySelector(".loader").style.filter = "alpha(opacity=" + opacity * 100 + ")";
+            opacity = opacity - opacity * 0.1;
+        }, 40);
+    }, 1500);
+    initialize();
+    
+}
