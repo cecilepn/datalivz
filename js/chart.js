b6da9bcef4e1f42a232c27e08c002faab76150a2
Cécile Phan Nguyen <cecilephan@outlook.com>
2023-11-19 17:44:03 +0100

    adding function on section graph1 + css

--- js/chart.js
+++ js/chart.js
@@ -8,7 +8,8 @@
     // Extraction des données : année et nombre d'entrées
     const data = jsonData.map(item => ({
         year: item.annee,
-        count: item.entrees
+        count: item.entrees,
+        explaination: item.desc
     }));
 
     // Création du graphique avec chart.js
@@ -17,17 +18,21 @@
         labels: [],
         datasets: [
             {
-                label: 'Entrées par année',
+                label: 'Entrées par année en millions',
                 data: [],
                 borderColor: '#03C7F7',
-                borderWidth: 3,
-
+                borderWidth: 2,
+                Radius: 4,
+                pointRadius: 6,
+                pointHoverRadius: 10,
+                pointHoverBackgroundColor: '#03C7F7',
             },
         ],
     };
 
     const graphOptions = {
         responsive: true,
+        maintainAspectRatio: false,
         scales: {
             yAxes: [
                 {
@@ -66,14 +71,23 @@
         } else {
             clearInterval(animationInterval); // Arrête l'animation à la fin des données
         }
+
+        // Fonction changeant la couleur des points dont l'année est clé uniquement
+        graphData.datasets[0].pointBackgroundColor = graphData.labels.map(year => {
+            if ([1943, 1944, 1945, 1946, 1947, 1950, 1952, 1953, 1957, 1958, 1971, 1984, 1990, 1997, 2001, 2005, 2009, 2010, 2015, 2019, 2020, 2022].includes(parseInt(year))) {
+                return '#03C7F7';  // Couleur rouge pour les années spécifiques
+            } else {
+                return '#000000';  // Couleur par défaut
+            }
+        });
     }
 
     // Mettre à jour le graphique toutes les x millisecondes (soit toutes les secondes)
     animationInterval = setInterval(updateGraphAndInfo, 1000);
 
-    // Gestionnaire de clic sur le bouton "Arrêter l'animation"
+    // Arrête l'animation
     document.getElementById('stopAnimationButton').addEventListener('click', () => {
-        clearInterval(animationInterval); // Arrête l'animation
+        clearInterval(animationInterval);
 
         // Afficher le graphique complet en mettant à jour les données une dernière fois
 
@@ -92,7 +106,8 @@
                 graph1.canvas.style.cursor = 'default';
             }
         });
-        // Afficher les données du point cliqué dans la div avec l'id "info"
+
+        // Affiche les données du point cliqué dans la div avec l'id "info"
         graph1.canvas.addEventListener('click', (event) => {
             const activePoints = graph1.getElementsAtEvent(event);
 
@@ -100,17 +115,21 @@
                 const clickedIndex = activePoints[0]._index;
                 const clickedYear = graphData.labels[clickedIndex];
                 const clickedCount = graphData.datasets[0].data[clickedIndex];
+                const explaination = data[clickedIndex].explaination;
 
                 document.getElementById('currentYear').textContent = `${clickedYear}`;
-                document.getElementById('currentCount').textContent = `${clickedCount} millions`;
+                document.getElementById('currentCount').textContent = `${clickedCount}`;
+                document.getElementById('explication').textContent = `${explaination}`;
+
             }
+
+
         });
 
         // Réinitialiser le curseur lorsque la souris quitte le graphique
         graph1.canvas.addEventListener('mouseout', () => {
             graph1.canvas.style.cursor = 'default';
         });
-
 
     });
 
